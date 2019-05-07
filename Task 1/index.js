8c9512788e72ad5b2b14a4c7748e037bf2683e75
vcka <vycioxas@gmail.com>
2019-05-07 21:35:04 +0300

    Task 6

--- Task 1/index.js
+++ Task 1/index.js
@@ -4,3 +4,20 @@ var app = new Vue({
     message: 'Hello Vue!'
   }
 })
+
+var app2 = new Vue({
+  el: '#app-2',
+  data: {
+    message: 'You loaded this page on ' + new Date().toLocaleString()
+  }
+})
+var app4 = new Vue({
+  el: '#app-4',
+  data: {
+    todos: [
+      { text: 'Learn JavaScript' },
+      { text: 'Learn Vue' },
+      { text: 'Build something awesome' }
+    ]
+  }
+})
