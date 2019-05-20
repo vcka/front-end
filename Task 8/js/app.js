c546d00945b9342a5b00403146dae300bc2e61c1
vcka <vycioxas@gmail.com>
2019-05-20 18:14:56 +0300

    Calculator

--- Task 8/js/app.js
+++ Task 8/js/app.js
@@ -1,17 +1,27 @@
 document.addEventListener('click', function (event) {
+  math(event);
+}, false);
+
+function math(event){
   var first = document.querySelector('#firstNum').value;
   var second = document.querySelector('#secondNum').value;
 
-if (event.target.matches('#sum')) {
-  alert("Sum is " + (+first + +second))
-}else if (event.target.matches('#sub')) {
-  alert("Sub is " + (+first - +second))
-}else if (event.target.matches('#div')) {
-  alert("Div is " + (+first / +second))
-}else if (event.target.matches('#mul')) {
-  alert("Mul is " + (+first * +second))
-}else if (event.target.matches('#mod')) {
-  alert("Mod is " + (+first % +second))
-};
-
-}, false);
+  switch (event.target.name) {
+    case 'sum':
+      alert("Sum is " + (+first + +second))
+      break;
+    case 'sub':
+      alert("Sub is " + (+first - +second))
+      break;
+    case 'div':
+      alert("Div is " + (+first / +second))
+      break;
+    case 'mul':
+      alert("Mul is " + (+first * +second))
+      break;
+    case 'mod':
+      alert("Mod is " + (+first % +second))
+      break;
+    default:
+  }
+}
