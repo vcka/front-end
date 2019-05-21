d588b13d9c21ae2575c75046096dae3d63683e3c
vcka <vycioxas@gmail.com>
2019-05-21 20:16:29 +0300

    a

--- Task 9/js/app.js
+++ Task 9/js/app.js
@@ -1,25 +1,93 @@
 $(function(){
-// function aClick() {
-//   $( "div" ).show().fadeOut( "slow" );
-// }
-// $( "#bind" ).click(function() {
-//   $( "body" )
-//     .delegate( "#theone", "click", aClick )
-//     .find( "#theone" ).text( "Can Click!" );
-// });
-// $( "#unbind" ).click(function() {
-//   $( "body" )
-//     .undelegate( "#theone", "click", aClick )
-//     .find( "#theone" ).text( "Does nothing..." );
-// });
-//
-// $( "#bind" ).bind( "click", function() {
-//   alert( "User clicked on 'foo.'" );
-// });
 
-$('button').click(function(e) {
-    var selected = $(this).attr('class');
-   alert('You clicked on button: ' + selected);
-});
+var item = null;
+  $('button').click(function(e) {
+      var selected = $(this).attr('class');
+      $('#saved').append(`
+      <li class="list-group-item">${item}</li>
+      `)
+  });
+
+  $('#generated li h1').click(function(e) {
+      alert($(this).text())
+      var selected = $(this).attr('class');
+      $('#saved').append(`
+      <li class="list-group-item">${item}</li>
+      `)
+  });
+
+  $.ajax({
+    method: 'GET',
+    url: 'https://api.chucknorris.io/jokes/categories'
+  }).done(function(data){
+    $.each(data,function (id, name) {
+      $('.categories').append(`
+      <option>${name}</option>
+      `)
+    });
+  })
+
+  function getJoke(category){
+    $.ajax({
+      method: 'GET',
+      url: `https://api.chucknorris.io/jokes/random?category=${category}`
+    }).done(function(data){
+      $.each(data,function (key, value) {
+        if (key === 'value') {
+          item = value;
+          console.log(value+" "+saved)
+          $('#generated').html(`
+          <li class="list-group-item"><h1>${value}</h1></li>
+          `)
+         }
+      });
+    })
+  }
+
+  function getSearchJoke(query){
+    $.ajax({
+      method: 'GET',
+      url: `https://api.chucknorris.io/jokes/search?query=${query}`
+    }).done(function(data){
+      $('#generated').html(``)
+      $.each(data.result,function (key, value) {
+        if (value.value != null) {
+          item = value;
+          var click = $('#generated').append(`
+          <li class="list-group-item"><h1>${value.value}</h1></li>
+          `)
+          click.click(function(e) {
+              alert($(this).text())
+              var selected = $(this).attr('class');
+              $('#saved').append(`
+              <li class="list-group-item">${value.value}</li>
+              `)
+          });
+        }
+      });
+    })
+  }
+
+  $('select').change(function() {
+    $("select option[value='remove']").remove();
+      var str = "";
+      $('select option:selected').each(function() {
+        str += $( this ).text() + " ";
+      });
+      getJoke(str);
+    })
+
+
+  $(':text').on('input', function(){
+    if($(this).val().length > 2) {
+         getSearchJoke(this.value);
+    }
+  });
+
+  // $('#generated').bootpag({
+  //   total: 10
+  // }).on("page", function(event, /* page number here */ num) {
+  //   $("generated").html("Insert content"); // some ajax content loading...
+  // });
 
 })
